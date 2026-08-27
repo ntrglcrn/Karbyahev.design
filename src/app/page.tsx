4d2ed1fe5d9648b638297d1c88a15cd853756820
Alexandr <alexandr@DESKTOP-O5R3DCH.viled.local>
2026-08-27 16:13:51 +0500

    feat: hand off hero into Viled project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/hero";
-import SelectedWork from "@/components/selected-work";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <SelectedWork />
     </main>
   );
 }
